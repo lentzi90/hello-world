04ba700dfd79f47ae63199d807d482e893f41771
lentzi90 <lennart.jern@gmail.com>
2014-10-10 21:08:30 +0300

    Added user input and greeting of user.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "And hello me!" << endl;
+    cout << "Hello world!\n";
+    cout << "What is your name sir?" << endl;
+    string name;
+    getline(cin, name); // använd getline för att inte avbryta inmatning vid space
+    cout << "Hello " << name << " nice to meet you!\n";
     return 0;
 }
