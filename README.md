6f8b65be15cf84f9900278aa56792f11ebe1aa8b
Lennart Jern <lennart.jern@gmail.com>
2017-03-08 16:41:13 +0200

    Test commit by upload
    Uploading new versions of README.md and src/main.cpp

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ hello-world
 This is my first repository, thus named hello-world.
 Git/GitHub is nice!
 
-I got Git working as well as Code::Blocks! Nice!
--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {
     cout << "Hello world!\n";
-    cout << "What is your name sir?" << endl;
+    cout << "What is your name?" << endl;
     string name;
-    getline(cin, name); // använd getline för att inte avbryta inmatning vid space
+    getline(cin, name); // use getline to avoid stopping at first space
     cout << "Hello " << name << " nice to meet you!\n";
     return 0;
 }
